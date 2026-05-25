7095768d96c75c0beaa0485af5abd98e0724985f
Lola Torillec <lolatorillec@gmail.com>
2026-05-25 12:06:34 +0200

    Add photo service tests and clean generated tests

--- backend/src/photos/photos.service.spec.ts
+++ backend/src/photos/photos.service.spec.ts
@@ -1,15 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PhotosService } from './photos.service';
+import { PrismaService } from '../prisma.service';
 
 describe('PhotosService', () => {
   let service: PhotosService;
 
+  const mockPrismaService = {
+    photo: {
+      findUnique: jest.fn(),
+      delete: jest.fn(),
+      update: jest.fn(),
+      create: jest.fn(),
+      findMany: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PhotosService],
+      providers: [
+        PhotosService,
+        {
+          provide: PrismaService,
+          useValue: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<PhotosService>(PhotosService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
